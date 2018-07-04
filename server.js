55ef2d8cd0d624fd4c4634b8680b99f3977d6ef0
Gabriel Pozzebom <gabriel.almeida.poa@hotmail.com>
2018-07-04 11:53:45 -0300

    added cors3

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ var express = require('express'),
 
 app = express(),
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-}),
-
 port = process.env.PORT || 3000,
 
 mongoose = require('mongoose'),
