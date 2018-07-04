9e77cf9e283c7a02e10723b9bf5534c8dcae46c1
Gabriel Pozzebom <gabriel.almeida.poa@hotmail.com>
2018-07-04 11:44:30 -0300

    added cors2

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express'),
 
 app = express(),
 
-app.use(cors()),
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}),
 
 port = process.env.PORT || 3000,
 
